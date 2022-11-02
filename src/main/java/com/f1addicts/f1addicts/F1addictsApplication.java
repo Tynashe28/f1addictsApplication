e27c970a5a8df669f9ff4a1c00d33829e17d1c2e
Tinashe Mabika <tmabika13@gmail.com>
2022-11-02 18:20:52 +0200

    Project Initialization

---
+++ src/main/java/com/f1addicts/f1addicts/F1addictsApplication.java
@@ -0,0 +1,13 @@
+package com.f1addicts.f1addicts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1addictsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1addictsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f1addicts/f1addicts/F1addictsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1addicts.f1addicts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1addictsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
